e178d098c77729d8ac1be934d0c54384c9f14b4c
julio <juli0o79@hotmail.com>
2021-02-10 14:44:31 -0300

    Alguns testes e anotações dos estudos com react native

--- App.js
+++ App.js
@@ -1,23 +1,59 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
+import {
+  Alert,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+  Platform,
+} from "react-native";
+import { Container, Text, Image, Button, ButtonText } from "./styles";
 export default function App() {
-  console.log("oi");
-  let x = 1;
+  const dealWithTouch = () => {
+    console.log("imagem tocada");
+    /* o style, ali na View, recebe um objeto. No caso, vc pode referenciar um objeto de uma variácel
+    como a const styles lá embaixo, ou vc pode chamar um objeto direto com as características desejadas.
+    No caso, vc vai abrir outra chaves para escrever um objeto direto, ou apenas referenciar a propriedade de styles, que é
+    um objeto ver 1:10:46 para entender melhor*/
+    // stylesheet.create assegura que as  propriedas do objeto estão corretas
+    //No caso do obeto javascript puro, o native não aponta um erro
+    //Você também, pode colocar uma array e referenciar mais de um objeto
+  };
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <Container>
+      <View style={{ backgroundColor: "none" }}></View>
+      <Text onPress={() => console.log("clicado")}>Helloo World!</Text>
+      <Image source={require("./assets/splash.png")} />
+      <TouchableWithoutFeedback onPress={dealWithTouch}>
+        <Image source={{ uri: "https://picsum.photos/200/300" }} />
+      </TouchableWithoutFeedback>
+      <Button
+        onPress={() =>
+          Alert.alert("Título da mensagem", "mensagem", [
+            { text: "Sim", onPress: () => console.log("Acesso permitido.") },
+            { text: "Não", onPress: () => console.log("Acesso negado.") },
+          ])
+        }
+      >
+        <ButtonText>Sou um botão</ButtonText>
+      </Button>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.PlatContainer}></View>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#1c1c1c",
     alignItems: "center",
     justifyContent: "center",
+    color: "white",
+  },
+  PlatContainer: {
+    backgroundColor: Platform.OS === "android" ? "blue" : "red",
+    height: 25,
+    width: 100,
   },
 });
---
+++ styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  color: white;
+  background-color: red;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SubContainer = styled.View``;
+
+export const Text = styled.Text`
+  color: white;
+`;
+
+export const Image = styled.Image`
+  width: 100px;
+  height: 100px;
+`;
+
+export const ButtonText = styled.Text`
+  color: #1c1c1c;
+  font-size: 14px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  height: 30px;
+  width: 100px;
+  background-color: white;
+  color: black;
+  border-radius 5px;
+  margin-top: 20px;
+`;
